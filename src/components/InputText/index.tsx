1538b265914a6f34116812b265ff15508c0b4946
kyo9bo <45879786+kk3939@users.noreply.github.com>
2023-07-30 17:41:45 +0900

    create InputText

--- src/components/InputText/index.tsx
+++ src/components/InputText/index.tsx
@@ -4,26 +4,26 @@ import { styled } from "styled-components";
 type Props = {
   placeholder: string;
   value: string;
-  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   className?: string;
   style?: React.CSSProperties;
 };
 
-const Component = styled.textarea`
-  padding: 10px;
-  min-height: 120px;
-  min-width: 300px;
+const Component = styled.input`
+  padding: 5px 5px;
+  min-height: 30px;
+  min-width: 100px;
   font-size: 1rem;
   border: 2px solid #cbcbcb;
   border-radius: 5px;
   font-family: "メイリオ";
 `;
 
-const TextArea = (props: Props) => {
+const InputText = (props: Props) => {
   return (
     <Component
       style={props.style}
-      className="kyo9bo-ui__text-area"
+      className="kyo9bo-ui__input-text"
       placeholder={props.placeholder}
       value={props.value}
       onChange={props.onChange}
@@ -31,4 +31,4 @@ const TextArea = (props: Props) => {
   );
 };
 
-export default TextArea;
+export default InputText;
